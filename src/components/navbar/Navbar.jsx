b004ca9377c8fd408eb05a3cfc138267fa77b791
teemee <teemee.olusa@gmail.com>
2022-07-17 15:59:52 +0100

    remove h1

--- src/components/navbar/Navbar.jsx
+++ src/components/navbar/Navbar.jsx
@@ -16,7 +16,6 @@ const Navbar = () => {
         <div className={isScrolled ? 'navbar scrolled' : 'navbar'}>
             <div className="container">
                 <div className="left">
-                    <h1>big head</h1>
                     <img 
                     src="https://upload.wikimedia.org/wikipedia/commons/thumb/0/08/Netflix_2015_logo.svg/170px-Netflix_2015_logo.svg.png"
                     alt=""
