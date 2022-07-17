86e166a43282afe953b1e31f6bf6e6bf3e6556b6
teemee <teemee.olusa@gmail.com>
2022-07-17 15:54:54 +0100

    added H1 tag

--- src/components/navbar/Navbar.jsx
+++ src/components/navbar/Navbar.jsx
@@ -16,6 +16,7 @@ const Navbar = () => {
         <div className={isScrolled ? 'navbar scrolled' : 'navbar'}>
             <div className="container">
                 <div className="left">
+                    <h1>big head</h1>
                     <img 
                     src="https://upload.wikimedia.org/wikipedia/commons/thumb/0/08/Netflix_2015_logo.svg/170px-Netflix_2015_logo.svg.png"
                     alt=""
